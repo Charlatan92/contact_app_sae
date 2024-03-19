a514476bb624844edec4429aca2976841e0306c6
Charlatan92 <xcharlatan92@gmail.com>
2024-03-19 14:13:27 +0100

    Serialisation

---
+++ AppContact/serializer.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from AppContact.models import Contact, ContactGroupe, Groupe
+
+class ContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Contact
+        fields=('id','prenom','nom','email','telephone','adresse','metier')
+        
+        
+class GroupeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Groupe
+        fields=('id','nom','couleur')
+        
+        
+class ContactGroupeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=ContactGroupe
+        fields=('id','contact_id','groupe_id')
